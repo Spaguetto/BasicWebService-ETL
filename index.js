5eeeacf41c3e0a4d89b98905e1143a34c1616fd6
CodeSandbox <support@codesandbox.io>
2023-11-22 23:23:46 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello CodeSandbox");
+const fs = require("fs");
+const csv = require("csv-parser");
+
+const inputFilePath = "./files/file.csv";
+const outputFilePath = "./files/file.json";
+
+const results = [];
+
+fs.createReadStream(inputFilePath)
+  .pipe(csv())
+  .on("data", (data) => {
+    results.push(data); // Almacena cada fila del CSV en el array 'results'
+  })
+  .on("end", () => {
+    // Convierte el array de resultados a formato JSON
+    const jsonData = JSON.stringify(results, null, 2);
+
+    // Escribe los datos JSON en un archivo de salida
+    fs.writeFileSync(outputFilePath, jsonData);
+
+    console.log("¡Conversión de CSV a JSON completada!");
+
+    console.log("wenas!");
+  });
